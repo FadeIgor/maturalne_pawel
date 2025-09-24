0b5b81fd47ece0de74631c99af399dbd10438c08
IW <122602491+FadeIgor@users.noreply.github.com>
2025-09-24 08:36:00 +0200

    Update maturalne.py

--- maturalne.py
+++ maturalne.py
@@ -1,1 +1,12 @@
+#ustawiam klucz na 1 element w liscie i jesli klucz jest wiekszy od kazdego pojedynczego elemneyu bedacegho po prawej stronie to je przenosze na lewa strone klucza 
+#hezeli natomiast po prawej stronie klucza elementy sa wieksze od niego to warunek nie zostaje spelniony i nie zamieniam elementow miejscami, zostae po prawej stronie klucza
 
+#TOK ROZUMOWANIA
+
+def przestaw(A):
+  klucz =A[0]
+  w = 0
+
+  for k in range(1, len(A)):
+    if A[k] < klucz:
+    
